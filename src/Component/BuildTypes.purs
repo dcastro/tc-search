eec827a58c626deede880f875e46b48e9e3e9ee3
dcastro <dc@diogocastro.com>
2017-02-02 14:16:24 +0000

    Render build types

--- src/Component/BuildTypes.purs
+++ src/Component/BuildTypes.purs
@@ -3,17 +3,18 @@ module Component.BuildTypes where
 import Prelude
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Network.HTTP.Affjax as AX
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Argonaut (decodeJson)
-import Data.Array (length)
+import Data.Array (singleton)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (un)
-import Model (BuildType, BuildTypes(..))
+import Model (BuildType(..), BuildTypes(..))
 
 
 type State =
@@ -46,7 +47,19 @@ render s =
       , case s.result of
           Nothing         -> HH.text "loading"
           Just (Left err) -> HH.text $ "Failed to load build types: " <> err
-          Just (Right xs) -> HH.text $ show $ length xs
+          Just (Right xs) -> renderBuildTypes xs
+    ]
+
+renderBuildTypes :: Array BuildType -> H.ComponentHTML Query
+renderBuildTypes xs =
+  HH.ul_ (HH.li_ <<< singleton <<< renderBuildType <$> xs)
+
+renderBuildType :: BuildType -> H.ComponentHTML Query
+renderBuildType (BuildType x) =
+  HH.p_
+    [ HH.text x.project
+    , HH.text " > "
+    , HH.a [HP.href x.url, HP.target "_blank"] [HH.text x.name]
     ]
 
 eval :: forall eff. Query ~> H.ComponentDSL State Query (Aff (Effects eff))
