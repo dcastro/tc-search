a5456da88553d7755ea56d0c326f334b3400a5f3
dcastro <dc@diogocastro.com>
2017-02-02 14:16:27 +0000

    Cache build types in the proxy's memory

--- tc-proxy/src/Main.purs
+++ tc-proxy/src/Main.purs
@@ -1,12 +1,13 @@
 module Main where
 
+import Control.Monad.Aff.Console as AC
 import Network.HTTP.Affjax as AX
-import Control.Monad.Aff.Class (liftAff)
+import Control.Monad.Aff (Aff, later', runAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION, Error, message)
-import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Ref (REF, Ref, newRef, readRef, writeRef)
 import Data.Int (fromString)
 import Data.Maybe (fromMaybe)
 import Data.MediaType.Common (applicationJSON)
@@ -24,20 +25,30 @@ import Node.HTTP (Server)
 import Node.Process (PROCESS, lookupEnv)
 import Prelude hiding (apply)
 
-getBuildTypes :: forall e. Handler (ajax :: AJAX |e)
-getBuildTypes = do
+type AppStateData = String
+type AppState     = Ref AppStateData
+
+initState :: forall e. Eff ( "ref" :: REF | e) (Ref String)
+initState = newRef "{ \"buildType\" : [] }"
+
+getBuildTypes :: forall e. Aff ( "ajax" :: AJAX | e) String
+getBuildTypes =
   let req = AX.defaultRequest
               {
                 headers = [Accept applicationJSON]
-              , url = "http://localhost:8080/sample"
-              -- , url = "http://scifbuild01:81/guestAuth/app/rest/buildTypes"
+              -- , url = "http://localhost:8080/sample"
+              , url = "http://scifbuild01:81/guestAuth/app/rest/buildTypes"
               }
-  rsp <- liftAff $ AX.affjax req
+  in  AX.affjax req <#> _.response
+
+-- Endpoints
+getBuildTypesHandler :: forall e. AppState -> Handler (ajax :: AJAX, ref :: REF | e)
+getBuildTypesHandler state = do
   setResponseHeader "Content-Type" applicationJSON
   setResponseHeader "Access-Control-Allow-Origin" "*"
-  send (rsp.response :: String)
+  (liftEff $ readRef state) >>= send
 
-sample :: forall e. Handler (fs :: FS, err :: EXCEPTION |e)
+sample :: forall e. Handler (fs :: FS, err :: EXCEPTION | e)
 sample = do
   json <- liftEff $ readTextFile UTF8 "sample.json"
   send json
@@ -58,12 +69,12 @@ errorHandler err = do
   setStatus 400
   sendJson {error: message err}
 
-appSetup :: forall e. App (ref :: REF, console :: CONSOLE , ajax :: AJAX, err :: EXCEPTION, fs :: FS | e)
-appSetup = do
+appSetup :: forall e. AppState -> App (ref :: REF, console :: CONSOLE , ajax :: AJAX, err :: EXCEPTION, fs :: FS | e)
+appSetup state = do
   liftEff $ log "Setting up"
   setProp "json spaces" 4.0
   use               logger
-  get "/buildTypes" getBuildTypes
+  get "/buildTypes" (getBuildTypesHandler state)
   get "/sample"     sample
   useOnError        errorHandler
 
@@ -73,6 +84,17 @@ main :: forall e. Eff (ref :: REF, express :: EXPRESS,
                        fs :: FS | e)
                       Server
 main = do
-  port <- (parseInt <<< fromMaybe "8080") <$> lookupEnv "PORT"
-  listenHttp appSetup port \_ ->
-    log $ "Listening on " <> show port
+  port <- liftEff $ (parseInt <<< fromMaybe "8080") <$> lookupEnv "PORT"
+  state <- initState
+  runAff logShow logShow $ setInterval 2000 $ do
+    AC.log "Updating build types"
+    str <- getBuildTypes
+    liftEff $ writeRef state str
+    AC.log "Updated build types"
+  liftEff (listenHttp (appSetup state) port \_ ->
+    log $ "Listening on " <> show port)
+
+setInterval :: forall e a. Int -> Aff e a -> Aff e Unit
+setInterval ms a = later' ms $ do
+  a
+  setInterval ms a
