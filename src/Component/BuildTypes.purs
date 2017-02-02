d2c8eb87a9e531e23743eea3844f16c0358d3015
dcastro <dc@diogocastro.com>
2017-02-02 14:16:24 +0000

    Fetch and deserialize build types

--- src/Component/BuildTypes.purs
+++ src/Component/BuildTypes.purs
@@ -5,22 +5,30 @@ import Halogen as H
 import Halogen.HTML as HH
 import Network.HTTP.Affjax as AX
 import Control.Monad.Aff (Aff)
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Aff.Class (liftAff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Argonaut (decodeJson)
+import Data.Array (length)
+import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
-import Model (BuildType)
+import Data.Newtype (un)
+import Model (BuildType, BuildTypes(..))
 
 
 type State =
   { searchText :: String
-  , buildTypes :: Maybe (Array BuildType)
+  , result :: Maybe Result
   }
+
+type Result = Either String (Array BuildType)
 
 type Effects eff = (ajax :: AX.AJAX, console :: CONSOLE | eff)
 
 data Query a = Initialize a
 
 initialState :: State
-initialState = { searchText: "", buildTypes: Nothing }
+initialState = { searchText: "", result: Nothing }
 
 ui :: forall eff. H.Component State Query (Aff (Effects eff))
 ui = H.lifecycleComponent
@@ -34,8 +42,17 @@ render :: State -> H.ComponentHTML Query
 render s =
   HH.div_
     [
-      HH.text "hello"
+      HH.p_ [HH.text "hello"]
+      , case s.result of
+          Nothing         -> HH.text "loading"
+          Just (Left err) -> HH.text $ "Failed to load build types: " <> err
+          Just (Right xs) -> HH.text $ show $ length xs
     ]
 
 eval :: forall eff. Query ~> H.ComponentDSL State Query (Aff (Effects eff))
-eval (Initialize next) = pure next
+eval (Initialize next) = do
+  H.liftH $ liftEff $ log "initializing"
+  response <- H.liftH $ liftAff $ AX.get "http://localhost:8080/buildTypes"
+  let result = un BuildTypes <$> decodeJson response.response
+  H.modify (_ { result = Just result })
+  pure next
--- src/Model.purs
+++ src/Model.purs
@@ -1,5 +1,8 @@
 module Model where
 
+import Prelude
+import Data.Newtype (class Newtype)
+import Data.Argonaut (class DecodeJson, decodeJson, (.?))
 import Network.HTTP.Affjax (URL)
 
 newtype BuildType = BuildType
@@ -7,3 +10,20 @@ newtype BuildType = BuildType
   , name :: String
   , project :: String
   }
+
+newtype BuildTypes = BuildTypes (Array BuildType)
+derive instance newtypeBuildTypes :: Newtype BuildTypes _
+
+instance decodeBuildType :: DecodeJson BuildType where
+  decodeJson json = do
+    obj <- decodeJson json
+    url <- obj .? "webUrl"
+    name <- obj .? "name"
+    project <- obj .? "projectName"
+    pure $ BuildType { url, name, project }
+
+instance decodeBuildTypes :: DecodeJson BuildTypes where
+  decodeJson json = do
+    obj <- decodeJson json
+    xs <- obj .? "buildType"
+    pure $ BuildTypes xs
